14fec55aff999a982dd8e5cd37ee3e9f13aefa63
Safwan Parker <safwan.parker@ingka.com>
2024-09-15 21:31:14 +0200

    chore: create intial pages and content config

---
+++ src/content/config.ts
@@ -0,0 +1,37 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean()
+  })
+});
+
+const work = defineCollection({
+  type: 'content',
+  schema: z.object({
+    company: z.string(),
+    role: z.string(),
+    dateStart: z.coerce.date(),
+    dateEnd: z.union([z.coerce.date(), z.string()])
+  })
+});
+
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean(),
+    demoURL: z.string().optional(),
+    repoURL: z.string().optional()
+  })
+});
+
+const collections = { blog, work, projects };
+
+export default collections;
