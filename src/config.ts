ed9f5e477c2e7ce21a2f0512f81f846788ccf789
Safwan Parker <safwan.parker@ingka.com>
2024-09-15 21:30:19 +0200

    chore: create initial site config

---
+++ src/config.ts
@@ -0,0 +1,46 @@
+import type { Site, Metadata, Socials } from '@/types';
+
+const SITE: Site = {
+  NAME: 'Safwan Parkar',
+  EMAIL: 'safwanparkar6@gmail.com',
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3
+};
+
+const HOME: Metadata = {
+  TITLE: 'Home',
+  DESCRIPTION: 'Welcome to my personal website.'
+};
+
+const BLOG: Metadata = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'All of my articles/posts on topics I am passionate about.'
+};
+
+const WORK: Metadata = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Where I have worked and what I have done.'
+};
+
+const PROJECTS: Metadata = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'All of my projects, with links to resources.'
+};
+
+const SOCIALS: Socials = [
+  {
+    NAME: 'twitter-x',
+    HREF: 'https://twitter.com/debingalooo'
+  },
+  {
+    NAME: 'github',
+    HREF: 'https://github.com/safwanyp'
+  },
+  {
+    NAME: 'linkedin',
+    HREF: 'https://www.linkedin.com/in/safwanyp'
+  }
+];
+
+export { SITE, HOME, BLOG, WORK, PROJECTS, SOCIALS };
---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+type Site = {
+  NAME: string;
+  EMAIL: string;
+  NUM_POSTS_ON_HOMEPAGE: number;
+  NUM_WORKS_ON_HOMEPAGE: number;
+  NUM_PROJECTS_ON_HOMEPAGE: number;
+};
+
+type Metadata = {
+  TITLE: string;
+  DESCRIPTION: string;
+};
+
+type Socials = {
+  NAME: string;
+  HREF: string;
+}[];
+
+export type { Site, Metadata, Socials };
