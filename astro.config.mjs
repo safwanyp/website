ad8c16992e0ed6c1f59670c024887911965e7237
Safwan Parker <safwan.parker@ingka.com>
2024-09-29 01:48:38 +0200

    build: update config for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://safwanyp.vercel.app',
+  site: 'https://safwanyp.com',
   trailingSlash: 'ignore',
   integrations: [mdx(), sitemap(), tailwind()]
 });
